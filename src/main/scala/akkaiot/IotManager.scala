ea6a7814f4caef104d7b6c8cfdf9682635a73d18
Nguyen Tuan Hai <nthaitrx@gmail.com>
2019-11-13 11:28:25 +0100

    added comments

--- src/main/scala/akkaiot/IotManager.scala
+++ src/main/scala/akkaiot/IotManager.scala
@@ -10,91 +10,114 @@ import akka.cluster.client.ClusterClient.SendToAll
 import com.sandinh.paho.akka._
 import com.sandinh.paho.akka.MqttPubSub._
 
-object IotManager {
-  def props(clusterClient: ActorRef, numOfDevices: Int, mqttPubSub: ActorRef): Props = Props(
-    new IotManager(clusterClient, numOfDevices, mqttPubSub)
-  )
+/*
+    1. IoT
+        An IotManager actor which:
+            - instantiates a specified number of devices upon start-up
+            - subscribes to a MQTT pub-sub topic for the work requests
+            - sends received work requests via ClusterClient to the master cluster
+            - notifies Device actors upon receiving failure messages from Master actor
+            - forwards work results to the corresponding devices upon receiving them from ResultProcessor
+*/
 
-  case class Ok(work: Work)
-  case class NotOk(work: Work)
+object IotManager {
+    def props(clusterClient: ActorRef, numOfDevices: Int, mqttPubSub: ActorRef): Props = Props(
+        new IotManager(clusterClient, numOfDevices, mqttPubSub)
+    )
+
+    case class Ok(work: Work)
+    case class NotOk(work: Work)
 }
 
 class IotManager(clusterClient: ActorRef, numOfDevices: Int, mqttPubSub: ActorRef) extends Actor with ActorLogging {
-  import IotManager._
-  import context.dispatcher
+    import IotManager._
+    import context.dispatcher
 
-  private var idToActorMap = Map.empty[String, ActorRef]
-  private var actorToIdMap = Map.empty[ActorRef, String]
+    private var idToActorMap = Map.empty[String, ActorRef]
+    private var actorToIdMap = Map.empty[ActorRef, String]
 
-  val deviceTypes = List("thermostat", "lamp", "security-alarm")
-  def random = ThreadLocalRandom.current
+    val deviceTypes = List("thermostat", "lamp", "security-alarm")
+    def random = ThreadLocalRandom.current
 
-  mqttPubSub ! Subscribe(MqttConfig.topic, self)
+    // subscribes to a MQTT pub-sub topic for the work requests
+    mqttPubSub ! Subscribe(MqttConfig.topic, self)
 
-  override def preStart(): Unit = {
-    log.info("IoT Manager -> Creating devices ...")
+    // instantiates a specified number of devices upon start-up
+    override def preStart(): Unit = {
+        log.info("IoT Manager -> Creating devices ...")
 
-    (1 to numOfDevices).foreach { n =>
-      val deviceType = deviceTypes(random.nextInt(0, deviceTypes.size))
-      val deviceId = (1000 + n).toString
-      val deviceActor = context.actorOf(Device.props(deviceType, deviceId, mqttPubSub), s"$deviceType-$deviceId")
-      context.watch(deviceActor)
-      actorToIdMap += deviceActor -> deviceId
-      idToActorMap += deviceId -> deviceActor
+        (1 to numOfDevices).foreach { n =>
+            val deviceType = deviceTypes(random.nextInt(0, deviceTypes.size))
+            val deviceId = (1000 + n).toString
+            val deviceActor = context.actorOf(
+                    Device.props(deviceType, deviceId, mqttPubSub),
+                    s"$deviceType-$deviceId")
+            context.watch(deviceActor)
+            actorToIdMap += deviceActor -> deviceId
+            idToActorMap += deviceId -> deviceActor
+        }
+
+        log.info("IoT Manager -> Created {} devices!", numOfDevices)
     }
 
-    log.info("IoT Manager -> Created {} devices!", numOfDevices)
-  }
+    override def postStop(): Unit = log.info("IoT Manager -> Stopped")
 
-  override def postStop(): Unit = log.info("IoT Manager -> Stopped")
+    override def receive: Receive = {
+        // subscribes to a MQTT pub-sub topic for the work requests
+        case SubscribeAck(Subscribe(MqttConfig.topic, `self`, _)) => {
+            log.info("IoT Manager -> MQTT subscription to {} acknowledged", MqttConfig.topic)
+            context.become(ready)
+        }
 
-  override def receive: Receive = {
-    case SubscribeAck(Subscribe(MqttConfig.topic, `self`, _)) => {
-      log.info("IoT Manager -> MQTT subscription to {} acknowledged", MqttConfig.topic)
-      context.become(ready)
+        case x =>
+            log.info("IoT Manager -> ALERT: Problem receiving message ... {}", x)
     }
 
-    case x =>
-      log.info("IoT Manager -> ALERT: Problem receiving message ... {}", x)
-  }
+    def ready: Receive = {
+        // sends received work requests via ClusterClient to the master cluster
+        case msg: Message => {
+            val work = MqttConfig.readFromByteArray[Work](msg.payload)
+            log.info("IoT Agent -> Received MQTT message: {}-{} | State {} | Setting {}",
+                work.deviceType, work.deviceId, work.currState, work.currSetting)
 
-  def ready: Receive = {
-    case msg: Message => {
-      val work = MqttConfig.readFromByteArray[Work](msg.payload)
-      log.info("IoT Agent -> Received MQTT message: {}-{} | State {} | Setting {}", work.deviceType, work.deviceId, work.currState, work.currSetting)
+            log.info("IoT Manager -> Sending work to cluster master")
+            implicit val timeout = Timeout(5.seconds)
+            (clusterClient ? SendToAll("/user/master/singleton", work)) map {
+                case Master.Ack(_) => Ok(work)
+            } recover {
+                case _ => NotOk(work)
+            } pipeTo {
+                idToActorMap.getOrElse(work.deviceId, `self`)
+            }
+        }
 
-      log.info("IoT Manager -> Sending work to cluster master")
-      implicit val timeout = Timeout(5.seconds)
-      (clusterClient ? SendToAll("/user/master/singleton", work)) map {
-        case Master.Ack(_) => Ok(work)
-      } recover { case _ => NotOk(work)
-      } pipeTo {
-        idToActorMap.getOrElse(work.deviceId, `self`)
-      }
+        // forwards work results to the corresponding devices upon receiving them from ResultProcessor
+        case result @ WorkResult(workId, deviceType, deviceId, nextState, nextSetting) =>
+            idToActorMap.get(deviceId) match {
+                case Some(deviceActor) =>
+                    deviceActor forward result
+                    log.info("IoT Manager -> Work result forwarded to {}-{} ",
+                        deviceType, deviceId)
+                case None =>
+                    log.info("IoT Manager -> ALERT: {}-{} NOT in registry!",
+                        deviceType, deviceId)
+            }
+
+        case Terminated(deviceActor) =>
+            val deviceId = actorToIdMap(deviceActor)
+            log.info("IoT Manager -> ALERT: Device actor terminated! Device Id {} will be removed.", deviceId)
+            actorToIdMap -= deviceActor
+            idToActorMap -= deviceId
+
+        case Ok(work) =>
+            log.info("IoT Manager -> ALERT: Receive ack from Master but Device Id of {}-{} NOT in registry!",
+                work.deviceType, work.deviceId)
+
+        case NotOk(work) =>
+            log.info("IoT Manager -> ALERT: Did not receive ack from Master and Device Id of {}-{} NOT in registry!",
+                work.deviceType, work.deviceId)
+
+        case x =>
+            log.info("IoT Manager -> ALERT: Problem with received message ... {}", x)
     }
-
-    case result @ WorkResult(workId, deviceType, deviceId, nextState, nextSetting) =>
-      idToActorMap.get(deviceId) match {
-        case Some(deviceActor) =>
-          deviceActor forward result
-          log.info("IoT Manager -> Work result forwarded to {}-{} ", deviceType, deviceId)
-        case None =>
-          log.info("IoT Manager -> ALERT: {}-{} NOT in registry!", deviceType, deviceId)
-      }
-
-    case Terminated(deviceActor) =>
-      val deviceId = actorToIdMap(deviceActor)
-      log.info("IoT Manager -> ALERT: Device actor terminated! Device Id {} will be removed.", deviceId)
-      actorToIdMap -= deviceActor
-      idToActorMap -= deviceId
-
-    case Ok(work) =>
-      log.info("IoT Manager -> ALERT: Receive ack from Master but Device Id of {}-{} NOT in registry!", work.deviceType, work.deviceId)
-
-    case NotOk(work) =>
-      log.info("IoT Manager -> ALERT: Did not receive ack from Master and Device Id of {}-{} NOT in registry!", work.deviceType, work.deviceId)
-
-    case x =>
-      log.info("IoT Manager -> ALERT: Problem with received message ... {}", x)
-  }
 }
--- src/main/scala/akkaiot/Main.scala
+++ src/main/scala/akkaiot/Main.scala
@@ -16,98 +16,98 @@ import com.sandinh.paho.akka.MqttPubSub._
 
 object Main {
 
-  def main(args: Array[String]): Unit = {
+    def main(args: Array[String]): Unit = {
 
-    val defaultDevices = 20
+        val defaultDevices = 20
 
-    if (args.isEmpty) {
-      startBackend(2551, "backend")
-      Thread.sleep(5000)
+        if (args.isEmpty) {
+            startBackend(2551, "backend")
+            Thread.sleep(5000)
 
-      startBackend(2552, "backend")
+            startBackend(2552, "backend")
 
-      startWorker(0)
-      Thread.sleep(5000)
+            startWorker(0)
+            Thread.sleep(5000)
 
-      startIot(3001, defaultDevices)
+            startIot(3001, defaultDevices)
 
-    } else {
-      val port = args(0).toInt
-      val numOfDevices = if (args.length > 1) args(1).toInt else defaultDevices
+        } else {
+            val port = args(0).toInt
+            val numOfDevices = if (args.length > 1) args(1).toInt else defaultDevices
 
-      if (2000 <= port && port <= 2999)
-        startBackend(port, "backend")
-      else if (3000 <= port && port <= 3999)
-        startIot(port, numOfDevices)
-      else if (port == 0)
-        startWorker(port)
+            if (2000 <= port && port <= 2999)
+                startBackend(port, "backend")
+            else if (3000 <= port && port <= 3999)
+                startIot(port, numOfDevices)
+            else if (port == 0)
+                startWorker(port)
+        }
     }
-  }
 
-  def workTimeout = 10.seconds
+    def workTimeout = 10.seconds
 
-  def startBackend(port: Int, role: String): Unit = {
-    val conf = ConfigFactory.parseString(s"akka.cluster.roles=[$role]").
-      withFallback(ConfigFactory.parseString("akka.remote.netty.tcp.port=" + port)).
-      withFallback(ConfigFactory.load())
-    val system = ActorSystem("ClusterSystem", conf)
+    def startBackend(port: Int, role: String): Unit = {
+        val conf = ConfigFactory.parseString(s"akka.cluster.roles=[$role]").
+            withFallback(ConfigFactory.parseString("akka.remote.netty.tcp.port=" + port)).
+            withFallback(ConfigFactory.load())
+        val system = ActorSystem("ClusterSystem", conf)
 
-    system.actorOf(
-      ClusterSingletonManager.props(
-        Master.props(workTimeout),
-        PoisonPill,
-        ClusterSingletonManagerSettings(system).withRole(role)
-      ),
-      "master")
-  }
+        system.actorOf(
+            ClusterSingletonManager.props(
+                Master.props(workTimeout),
+                PoisonPill,
+                ClusterSingletonManagerSettings(system).withRole(role)
+            ),
+            "master")
+    }
 
-  def startIot(port: Int, numOfDevices: Int): Unit = {
-    val conf = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + port).
-      withFallback(ConfigFactory.load("iotmanager"))
-    val system = ActorSystem("IotSystem", conf)
-    val initialContacts = immutableSeq(conf.getStringList("contact-points")).map {
-      case AddressFromURIString(addr) => RootActorPath(addr) / "system" / "receptionist"
-    }.toSet
+    def startIot(port: Int, numOfDevices: Int): Unit = {
+        val conf = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + port).
+            withFallback(ConfigFactory.load("iotmanager"))
+        val system = ActorSystem("IotSystem", conf)
+        val initialContacts = immutableSeq(conf.getStringList("contact-points")).map {
+            case AddressFromURIString(addr) => RootActorPath(addr) / "system" / "receptionist"
+        }.toSet
 
-    val clusterClient = system.actorOf(
-      ClusterClient.props(
-        ClusterClientSettings(system)
-          .withInitialContacts(initialContacts)),
-      "clusterClient")
+        val clusterClient = system.actorOf(
+            ClusterClient.props(
+                ClusterClientSettings(system)
+                    .withInitialContacts(initialContacts)),
+            "clusterClient")
 
-    val mqttPubSub = system.actorOf(Props(classOf[MqttPubSub], MqttConfig.psConfig))
+        val mqttPubSub = system.actorOf(Props(classOf[MqttPubSub], MqttConfig.psConfig))
 
-    val iotManager = system.actorOf(IotManager.props(clusterClient, numOfDevices, mqttPubSub), "iot-manager")
+        val iotManager = system.actorOf(IotManager.props(clusterClient, numOfDevices, mqttPubSub), "iot-manager")
 
-    // Start ResultProcessor with random port
-    startResultProcessor(
-      resPort = 0,
-      iotPath = ActorPath.fromString(s"akka.tcp://IotSystem@127.0.0.1:${port}/user/iot-manager")
-    )
-  }
+        // Start ResultProcessor with random port
+        startResultProcessor(
+            resPort = 0,
+            iotPath = ActorPath.fromString(s"akka.tcp://IotSystem@127.0.0.1:${port}/user/iot-manager")
+        )
+    }
 
-  def startWorker(port: Int): Unit = {
-    val conf = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + port).
-      withFallback(ConfigFactory.load("worker"))
-    val system = ActorSystem("WorkerSystem", conf)
-    val initialContacts = immutableSeq(conf.getStringList("contact-points")).map {
-      case AddressFromURIString(addr) => RootActorPath(addr) / "system" / "receptionist"
-    }.toSet
+    def startWorker(port: Int): Unit = {
+        val conf = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + port).
+            withFallback(ConfigFactory.load("worker"))
+        val system = ActorSystem("WorkerSystem", conf)
+        val initialContacts = immutableSeq(conf.getStringList("contact-points")).map {
+            case AddressFromURIString(addr) => RootActorPath(addr) / "system" / "receptionist"
+        }.toSet
 
-    val clusterClient = system.actorOf(
-      ClusterClient.props(
-        ClusterClientSettings(system)
-          .withInitialContacts(initialContacts)),
-      "clusterClient")
+        val clusterClient = system.actorOf(
+            ClusterClient.props(
+                ClusterClientSettings(system)
+                    .withInitialContacts(initialContacts)),
+            "clusterClient")
 
-    system.actorOf(Worker.props(clusterClient, Props[WorkProcessor]), "worker")
-  }
+        system.actorOf(Worker.props(clusterClient, Props[WorkProcessor]), "worker")
+    }
 
-  def startResultProcessor(resPort: Int, iotPath: ActorPath): Unit = {
-    val conf = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + resPort).
-      withFallback(ConfigFactory.load())
-    val system = ActorSystem("ClusterSystem", conf)
+    def startResultProcessor(resPort: Int, iotPath: ActorPath): Unit = {
+        val conf = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + resPort).
+            withFallback(ConfigFactory.load())
+        val system = ActorSystem("ClusterSystem", conf)
 
-    system.actorOf(ResultProcessor.props(iotPath), "result-processor")
-  }
+        system.actorOf(ResultProcessor.props(iotPath), "result-processor")
+    }
 }
